beb5536fdc74565846fbf43aa4545577c15c784a
flyinglobster <yahwistasalomo@gmail.com>
2024-11-05 15:28:58 +0700

    responder tidak bergantung clk lagi

--- i2c_master_axil/i2c_responder.sv
+++ i2c_master_axil/i2c_responder.sv
@@ -17,34 +17,33 @@ class i2c_responder extends uvm_component;
     endfunction
     
     task monitor_start_condition();
-        wait(!vif.sda_o && vif.scl_o);
+        @(negedge vif.sda_o iff vif.scl_o === 1);  
         `uvm_info("I2C_RESP", "START condition detected", UVM_LOW)
     endtask
     
     task receive_byte(output bit [7:0] data);
         for(int i = 7; i >= 0; i--) begin
-            @(posedge vif.clk);
-            wait(vif.scl_o);
+            @(posedge vif.scl_o);  
             data[i] = vif.sda_o;
-            @(negedge vif.clk);
+            @(negedge vif.scl_o);  
         end
     endtask
     
     task send_ack();
-        @(posedge vif.clk);
-        vif.sda_i <= 0;
-        @(posedge vif.clk);
-        vif.sda_i <= 1;
+        @(posedge vif.scl_o);
+        vif.sda_i <= 0;  
+        @(negedge vif.scl_o);
+        vif.sda_i <= 1;  
     endtask
     
     task send_byte(bit [7:0] data);
         for(int i = 7; i >= 0; i--) begin
-            @(posedge vif.clk);
+            @(negedge vif.scl_o);  
             vif.sda_i <= data[i];
-            @(negedge vif.clk);
+            @(posedge vif.scl_o);  
         end
-        // Release SDA for master ACK
-        vif.sda_i <= 1;
+        @(negedge vif.scl_o);
+        vif.sda_i <= 1;  
     endtask
 
     task run_phase(uvm_phase phase);
@@ -52,27 +51,22 @@ class i2c_responder extends uvm_component;
         bit [7:0] data_byte;
         bit is_read;
         
+        vif.sda_i <= 1;  
+        
         forever begin
-            // Wait for START
             monitor_start_condition();
             
-            // Receive address
             receive_byte(addr_byte);
             
-            // Check if this transaction is for us
             if((addr_byte[7:1] == my_address)) begin
                 is_read = addr_byte[0];
-                
-                // Send ACK
                 send_ack();
                 
                 if(is_read) begin
-                    // Read operation
                     data_byte = memory[addr_byte[7:1]];
                     send_byte(data_byte);
                     `uvm_info("I2C_RESP", $sformatf("Sending data: %h", data_byte), UVM_LOW)
                 end else begin
-                    // Write operation
                     receive_byte(data_byte);
                     memory[addr_byte[7:1]] = data_byte;
                     send_ack();
